7537fcf002ee72ab169994ff83254133f69fe87d
mikey <mikey@hey.com>
2023-06-01 09:57:37 -0500

    add django templating language

--- discogskiii/firstapp/views.py
+++ discogskiii/firstapp/views.py
@@ -6,4 +6,6 @@ def index(request):
     return render(request, "firstapp/index.html")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}")
+    return render(request, "firstapp/greet.html", {
+        "name": name.capitalize()
+    })
