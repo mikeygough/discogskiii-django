1f1761b36ae3caa983a1cced59fe822ad1bd07c0
mikey <mikey@hey.com>
2023-05-31 20:11:49 -0500

    add django resources. create project discogskiii

--- discogskiii/discogskiii/asgi.py
+++ discogskiii/discogskiii/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for discogskii project.
+ASGI config for discogskiii project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discogskii.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discogskiii.settings')
 
 application = get_asgi_application()
--- discogskiii/discogskiii/wsgi.py
+++ discogskiii/discogskiii/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for discogskii project.
+WSGI config for discogskiii project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discogskii.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discogskiii.settings')
 
 application = get_wsgi_application()
