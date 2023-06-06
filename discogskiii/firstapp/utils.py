2f45ebf6a107975b0bb52285f238093b01f2df1a
mikey <mikey@hey.com>
2023-06-06 13:38:02 -0500

    add get_master_id function to utils

--- discogskiii/firstapp/utils.py
+++ discogskiii/firstapp/utils.py
@@ -6,9 +6,33 @@ import json
 
 # base variables
 base_url = "https://api.discogs.com"
-headers = {
+authentication_header = {
     "Authorization": f"Discogs key={CONSUMER_KEY}, secret={CONSUMER_SECRET}",
 }
+
+
+def get_master_id(artist):
+    ''' REQUIRES AUTHENTICATION
+        returns master_id of first result from
+        discogs database artist search with specific query parameters'''
+    
+    # set query parameters
+    params = {
+    "artist": f"{artist}",
+    "type": "master",
+    "format": "vinyl",
+    "page": "1"
+    }
+    
+    response = requests.get(f"{base_url}/database/search",
+                            headers=authentication_header,
+                            params=params).text
+    # turn string into json
+    response_json = json.loads(response)
+    # get master_id
+    master_id = response_json["results"][0]["id"]
+    
+    return master_id
 
 
 def get_marketplace_listing(listing_id):
@@ -16,7 +40,7 @@ def get_marketplace_listing(listing_id):
         return marketplace listing json '''
     
     response = requests.get(f"{base_url}/marketplace/listings/{listing_id}",
-                    headers=headers).text
+                    headers=authentication_header).text
 
     # turn string into pretty json
     response_json = json.loads(response)
@@ -25,8 +49,12 @@ def get_marketplace_listing(listing_id):
     return response_json
 
 def main():
+    print("GET MASTER ID")    
+    master_id = get_master_id("sun ra")
+    print(master_id) # 143593
+    print("-------------------------------")
     print("get_marketplace_listing")
-    listing = get_marketplace_listing('2047141883')
+    listing = get_marketplace_listing("2047141883")
     print(listing)
     
 if __name__ == '__main__':
