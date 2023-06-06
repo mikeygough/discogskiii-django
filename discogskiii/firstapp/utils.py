6af583a921300b7dc8ee4cc60935911ada47c8c1
mikey <mikey@hey.com>
2023-06-06 13:53:00 -0500

    add get_main_release_id function

--- discogskiii/firstapp/utils.py
+++ discogskiii/firstapp/utils.py
@@ -35,6 +35,19 @@ def get_master_id(artist):
     return master_id
 
 
+def get_main_release_id(master_id):
+    ''' REQUIRES AUTHENTICATION
+        returns release_id of the master release'''
+    
+    response = requests.get(f"{base_url}/masters/{master_id}",
+                            headers=authentication_header).text
+    # turn string into json
+    response_json = json.loads(response)
+    # get release_id of master release
+    main_release_id = response_json["main_release"]
+    return main_release_id
+
+
 def get_marketplace_listing(listing_id):
     ''' REQUIRES AUTHENTICATION
         return marketplace listing json '''
@@ -49,9 +62,13 @@ def get_marketplace_listing(listing_id):
     return response_json
 
 def main():
-    print("GET MASTER ID")    
+    print("get_master_id('sun ra')")
     master_id = get_master_id("sun ra")
-    print(master_id) # 143593
+    print(master_id) # 143592
+    print("-------------------------------")
+    print(f"get_main_release_id({master_id})")
+    main_release_id = get_main_release_id(master_id)
+    print(main_release_id) # 7054180
     print("-------------------------------")
     print("get_marketplace_listing")
     listing = get_marketplace_listing("2047141883")
