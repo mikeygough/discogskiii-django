2ea57084f1b8e2700d190b6ca713d3f715f2268a
mikey <mikey@hey.com>
2023-06-07 14:09:29 -0500

    start adding models

--- discogskiii/firstapp/models.py
+++ discogskiii/firstapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class MainRelease(models.Model):
+    master_id = models.CharField()
+    title = models.CharField()
+    uri = models.CharField()
+    year = models.CharField()
+    thumb = models.CharField()
