2cafa99b8e554cfd0a77db61ed04bc524fee866e
mikey <mikey@hey.com>
2023-06-06 20:17:26 -0500

    begin homepage construction

--- discogskiii/firstapp/urls.py
+++ discogskiii/firstapp/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "firstapp"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet")
+    path("<str:artist>", views.amarkets, name="amarkets")
 ]
--- discogskiii/firstapp/views.py
+++ discogskiii/firstapp/views.py
@@ -1,11 +1,21 @@
-# from django.http import HttpResponse
+# imports
 from django.shortcuts import render
+
+
+artist_markets = [
+    "Sun Ra",
+    "John Coltrane",
+    "Miles Davis",
+    "Alice Coltrane"
+]
 
 # Create your views here.
 def index(request):
-    return render(request, "firstapp/index.html")
+    return render(request, "firstapp/index.html", {
+        "artist_markets": artist_markets
+    })
 
-def greet(request, name):
-    return render(request, "firstapp/greet.html", {
-        "name": name.capitalize()
+def amarkets(request, artist):
+    return render(request, "firstapp/artist_markets.html", {
+        "artist": artist
     })
