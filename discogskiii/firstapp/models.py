7fe770b53f6e1059431009e34ca6c23aba8c60c6
mikey <mikey@hey.com>
2023-06-09 13:18:24 -0500

    add MainRelease model

--- discogskiii/firstapp/models.py
+++ discogskiii/firstapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
 class MasterRelease(models.Model):
+    ''' represents the master pressing '''
     artist = models.CharField(max_length=240) 
     master_id = models.CharField(max_length=15)
     title = models.CharField(max_length=240)
@@ -11,3 +13,12 @@ class MasterRelease(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.artist}"
+
+
+class MainRelease(models.Model):
+    ''' represents the main release (original pressing) '''
+    master = models.ForeignKey(MasterRelease, on_delete=models.CASCADE)
+    main_id = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.main_id} is the original pressing of..."
