8aaf5315447cedecee5954928416944271b2e649
mikey <mikey@hey.com>
2023-06-13 15:32:14 -0500

    add async dependencies

--- discogskiii/firstapp/async_utils.py
+++ discogskiii/firstapp/async_utils.py
@@ -1,12 +1,29 @@
 # trying to convert utils.py into async functions
 # helper functions
-from firstapp.config import *
+from config import *
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import json
 import re
-import requests
 from datetime import datetime
 
-# import models
-from firstapp.models import MasterRelease
+# async libs
+import asyncio
+import aiohttp
+
+
+def async_get_main_release_id(master_id):
+    ''' REQUIRES AUTHENTICATION
+        given a master_id (record meta object), retun the release_id
+        of the original pressing. you can think of 'main_release' as the same
+        as 'original_pressing'. '''
+    
+    # get data as json
+    response_json = json.loads(requests.get(f"{API_BASE_URL}/masters/{master_id}",
+                            headers=AUTHENTICATION_HEADER).text)
+    
+    # get release_id of master release
+    main_release_id = response_json["main_release"]
+    
+    # return main_release_id (original_pressing_id)
+    return main_release_id
