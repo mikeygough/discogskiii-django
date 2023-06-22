b5d483ff27b6608b7e4ea6607249b426fefdff2b
mikey <mikey@hey.com>
2023-06-22 11:52:51 -0500

    add savedmarkets model to admin

--- discogskiii/firstapp/admin.py
+++ discogskiii/firstapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from firstapp.models import MasterRelease, MainRelease
+from firstapp.models import MasterRelease, MainRelease, SavedMarkets
 
 # Register your models here.
 admin.site.register(MasterRelease)
 admin.site.register(MainRelease)
+admin.site.register(SavedMarkets)
