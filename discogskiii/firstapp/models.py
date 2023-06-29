08700fb36b3ca247c1a5cc10506eb31c1587dde7
mikey <mikey@hey.com>
2023-06-28 19:22:58 -0500

    add notes about updating model field types

--- discogskiii/firstapp/models.py
+++ discogskiii/firstapp/models.py
@@ -11,9 +11,11 @@ class MasterRelease(models.Model):
     artist = models.CharField(max_length=240)
     master_id = models.CharField(max_length=15)
     title = models.CharField(max_length=240)
+    # is there maybe a URL field?
     uri = models.CharField(max_length=500)
     # think about changing this to models.DateField() since we order by it
     year = models.CharField(max_length=10)
+    # is there maybe a URL field?
     thumb = models.CharField(max_length=500)
 
     def __str__(self):
@@ -23,14 +25,18 @@ class MasterRelease(models.Model):
 class MainRelease(models.Model):
     ''' represents the main release (original pressing) '''
     main_id = models.CharField(max_length=15)
+    # is there maybe a URL field?
     uri = models.CharField(max_length=500)
     community_have = models.IntegerField(default=0)
     community_want = models.IntegerField(default=0)
     community_demand_score = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
     num_for_sale = models.IntegerField(null=True, default=0)
+    # should price really be a charfield?
     lowest_price = models.CharField(max_length=15, null=True)
     title = models.CharField(max_length=240)
+    # think about changing this to models.DateField() since we order by it
     released = models.CharField(max_length=10)
+    # is there maybe a URL field?
     thumb = models.CharField(max_length=500)
     master = models.ForeignKey(MasterRelease, on_delete=models.CASCADE)
 
